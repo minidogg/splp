7c14dbe84e3aca26cffb4f09b0e73be40d85115e
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-10-13 13:43:41 -0500

    I forgot to make a git repo 💀

---
+++ script.js
@@ -0,0 +1,125 @@
+//Automated by https://codepen.io/TheRealCrafterboy27/full/GRbPBeb
+const ELEMENTS = {
+  "fileInput":document.getElementById("fileInput"),
+  "controls":document.getElementById("controls"),
+  "current":document.getElementById("current"),
+  "time":document.getElementById("time"),
+  "play":document.getElementById("play"),
+  "pause":document.getElementById("pause"),
+  "next":document.getElementById("next"),
+  "shuffle":document.getElementById("shuffle"),
+  "status":document.getElementById("status"),
+  "volume":document.getElementById("volume"),
+  "playlist":document.getElementById("playlist"),
+}
+let tracks = []
+let currentTrack = -1;
+
+ELEMENTS.fileInput.addEventListener('change', async function selectedFileChanged() {
+    if (this.files.length === 0) {
+      console.log('No file selected.');
+      return;
+    }
+  
+
+    async function LoadDataUrlFromFile(file){
+      const reader = new FileReader();
+      reader.readAsDataURL(file)
+      return new Promise((r)=>{
+        reader.onload = ()=>{
+          r(reader.result);
+        }
+      })
+    }
+    
+
+    for(let i = 0;i<this.files.length;i++){
+      console.log(this.files[i])
+      let label = document.createElement('p')
+      label.textContent = this.files[i].name
+      ELEMENTS.playlist.appendChild(label)
+
+      let audioSrc = await LoadDataUrlFromFile(this.files[i])
+      let audio = document.createElement("audio")
+      audio.controls = false
+      audio.setAttribute("name", this.files[i].name)
+  
+      audio.src = audioSrc
+      tracks.push(audio)
+      ELEMENTS.playlist.appendChild(audio)
+    }
+    
+});
+
+function PlayNextTrack(){
+  if(tracks.length==0)return;
+  if(tracks[currentTrack]){
+    tracks[currentTrack].pause()
+    tracks[currentTrack].currentTime = 0;
+  };
+  currentTrack = (currentTrack+1)%tracks.length;
+  tracks[currentTrack].play()
+  ELEMENTS.current.textContent = tracks[currentTrack].getAttribute("name")
+
+  tracks[currentTrack].addEventListener("ended", PlayNextTrack)
+}
+ELEMENTS.next.addEventListener("click", PlayNextTrack)
+
+function UpdateStatus(){
+  if(tracks.length==0)return;
+  if(currentTrack==-1)PlayNextTrack()
+  
+  // Update time status
+  const date = new Date(null);
+  date.setSeconds(tracks[currentTrack].currentTime);
+  const timeString = date.toISOString().substr(11, 8);
+  date.setSeconds(tracks[currentTrack].duration);
+  const durationString = date.toISOString().substr(11, 8);
+  ELEMENTS.time.textContent = timeString + " / " + durationString;
+
+  ELEMENTS.status.textContent = tracks[currentTrack].paused?"Paused":"Playing"
+}
+setInterval(UpdateStatus, 100)
+
+ELEMENTS.play.addEventListener("click", ()=>{
+  if(tracks.length==0)return;
+  tracks[currentTrack].play()
+})
+ELEMENTS.pause.addEventListener("click", ()=>{
+  if(tracks.length==0)return;
+  tracks[currentTrack].pause()
+})
+ELEMENTS.volume.addEventListener("input", ()=>{
+  for(let track of tracks){
+    track.volume = ELEMENTS.volume.value/100;
+  }
+})
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+ELEMENTS.shuffle.addEventListener("click", ()=>{
+  if(tracks.length==0)return;
+  shuffle(tracks);
+  ELEMENTS.playlist.innerHTML = ""
+  tracks.forEach(e=>{
+    let label = document.createElement('p')
+    label.textContent = e.getAttribute("name")
+    ELEMENTS.playlist.appendChild(label)
+  })
+
+  currentTrack = -1;
+  PlayNextTrack();
+})
