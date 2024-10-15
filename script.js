eca3ce417ca5aaaaa241a4450c94995a29b4dcd5
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-10-14 21:16:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ async function AddAudio(file, dataUrl=undefined){
   }
 
   let audioSrc = dataUrl==undefined?await LoadDataUrlFromFile(file):dataUrl
-  audioSrc = LZString.compress(audioSrc)
+ //audioSrc = LZString.compress(audioSrc)
   // let audio = document.createElement("audio")
   // audio.controls = false
   // audio.setAttribute("name", file.name)
@@ -135,8 +135,8 @@ function PlayNextTrack(){
   StopAllTracks()
 
   currentTrack = (currentTrack+1)%tracks.length;
-  audioElement.src = LZString.decompress(tracks[currentTrack][1])
-
+  //audioElement.src = LZString.decompress(tracks[currentTrack][1])
+audioElement.src = tracks[currentTrack][1]
   audioElement.play()
   LoadVolumeChange()
   ELEMENTS.current.textContent = tracks[currentTrack][0]
