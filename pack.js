a7a9ceb51b7b4a01fb60f70b2cc3c68d9d032cd4
minidogg <117037081+minidogg@users.noreply.github.com>
2025-02-26 21:27:13 -0600

    repacked

--- pack.js
+++ pack.js
@@ -1,10 +1,11 @@
 const fs = require("fs")
 
 let index = fs.readFileSync("./index.html", "utf-8")
-// TODO: Automate this process
+// TODO: Automate fetching and replacing the linked css and js in the html. 
 index = index
     .replace('<link rel="stylesheet" href="style.css">', "<style>"+fs.readFileSync("./style.css", "utf-8")+"</style>")
-    .replace('<script src="/script.js"></script>', "<script>"+fs.readFileSync("./script.js", "utf-8")+"</script>");
+    .replace('<script src="/script.js"></script>', "<script>"+fs.readFileSync("./script.js", "utf-8")+"</script>")
+    .replace('<script src="/mutag.js"></script>', "<script>"+fs.readFileSync("./mutag.js", "utf-8")+"</script>");
 
 
 
