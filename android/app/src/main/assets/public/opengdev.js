e5c91eb31f32ce73764f8a80201f794ec25e8d9d
minidogg <117037081+minidogg@users.noreply.github.com>
2025-02-27 09:04:56 -0600

    eh just made it easier to download offline

--- android/app/src/main/assets/public/opengdev.js
+++
@@ -1,9 +0,0 @@
-// alert('b')
-// if(localStorage.getItem("dev")==true)OpenGDevTools()
-window.addEventListener("keypress", (ev)=>{
-    if(ev.ctrlKey==true && ev.shiftKey == true && ev.key=="L"){
-        // localStorage.setItem("dev", localStorage.getItem("dev")=="false")
-        // if(localStorage.getItem("dev")==true)OpenGDevTools()
-        OpenGDevTools()
-    }
-})
